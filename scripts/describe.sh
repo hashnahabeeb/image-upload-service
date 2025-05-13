cca3c7f3180a713679429732889341073038680a
hh001 <hashna.h@in.pega.com>
2025-05-14 00:42:46 +0530

    committing scripts to deploy

--- scripts/describe.sh
+++ scripts/describe.sh
@@ -2,7 +2,7 @@
 set -e
 
 STACK_NAME=image-upload-service
-#awslocal cloudformation describe-stacks --stack-name $STACK_NAME
-awslocal cloudformation describe-stack-events --stack-name $STACK_NAME \
-    --query "StackEvents[*].[ResourceType,ResourceStatus,ResourceStatusReason]" \
-    --output table
+awslocal cloudformation describe-stacks --stack-name $STACK_NAME
+#awslocal cloudformation describe-stack-events --stack-name $STACK_NAME \
+#    --query "StackEvents[*].[ResourceType,ResourceStatus,ResourceStatusReason]" \
+#    --output table

cbc5c9caaa24bde4825e59de554578de4365ff22
hh001 <hashna.h@in.pega.com>
2025-05-17 19:29:31 +0530

    upload image as binary

--- src/upload_image/lambda_function.py
+++ src/upload_image/lambda_function.py
@@ -7,13 +7,6 @@ import base64
 s3_client = boto3.client('s3')
 dynamodb_client = boto3.client('dynamodb')
 
-def parse_key_value_string(key_value_string):
-    """Parses a command-separated key:value string into a dictionary."""
-    try:
-        return dict(item.split(":") for item in key_value_string.split(","))
-    except ValueError:
-        raise ValueError("Invalid key:value format. Ensure the string is properly formatted.")
-
 
 def lambda_handler(event, context):
     bucket_name = os.environ['BUCKET_NAME']
@@ -29,30 +22,21 @@ def lambda_handler(event, context):
         content_type = event['headers'].get('Content-Type') or event['headers'].get('content-type')
         file_extension = content_type.split('/')[-1]
 
-        if not file_extension:
-            raise ValueError("Content-Type must specify a valid file extension (e.g., image/jpeg)")
-
-        # Generate a unique image ID and S3 key
         image_id = str(uuid.uuid4())
         image_key = f"images/{image_id}.{file_extension}"
 
         # Upload the binary image data to S3
         s3_client.put_object(Bucket=bucket_name, Key=image_key, Body=image_data, ContentType=content_type)
 
-        raw_metadata = event.get('queryStringParameters', {}).get('metadata', '')
-        metadata_dict = parse_key_value_string(raw_metadata)
         metadata = {
-            key: {'S': value} for key, value in metadata_dict.items()
+            "image_id": {'S': "image_id"},
+            "content_type": {'S': "content_type"}
         }
-        metadata["image_id"] = {'S': image_id}
-        metadata["content_type"] = {'S': content_type}
 
-        # Parse tags (optional)
         raw_tags = event.get('queryStringParameters', {}).get('tags', None)
         if raw_tags:
-            tags = raw_tags.split(",")  # Split tags by comma
-            metadata["tags"] = {'L': [{'S': tag.strip()} for tag in tags]}  # Store as a list of strings
-
+            tags = raw_tags.split(",")
+            metadata["tags"] = {'L': [{'S': tag.strip()} for tag in tags]}
 
         dynamodb_client.put_item(
             TableName=table_name,
@@ -73,5 +57,3 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps({'error': str(e)})
         }
-
-lambda_handler(None, None)
